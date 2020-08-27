2ece7a1b0aaa2938956625e719384dd6c2345596
Anders Børjesson <anbo@zealand.dk>
2020-08-27 11:59:05 +0200

    Add project files.

---
+++ TeacherLibrary/Teacher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TeacherLibrary
+{
+    public class Teacher
+    {
+        private int _salary;
+
+        public int Salary
+        {
+            get => _salary;
+            set
+            {
+                if (value < 0) throw new ArgumentOutOfRangeException();
+                _salary = value;
+            }
+        }
+
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ TeacherLibraryTests/TeacherTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TeacherLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeacherLibrary.Tests
+{
+    [TestClass()]
+    public class TeacherTests
+    {
+        [TestMethod]
+        public void SalaryTest()
+        {
+            Teacher teacher = new Teacher();
+            teacher.Salary = 100;
+            Assert.AreEqual(100, teacher.Salary);
+
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+                
+        } 
+    }
+}
